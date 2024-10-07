3edc12ad97d4bfc1fc3a5d5df90ffbb7fdb6e942
IbrahimFalihHassan <71994839+IbrahimFalihHassan@users.noreply.github.com>
2024-10-07 15:46:34 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const blindsDiv = document.getElementById("Blinds");
-
-window.scrollTo({ top: 0, behavior: 'smooth' });
-
-setInterval(removeBlinds, 3000);
-
-function removeBlinds() {
-    blindsDiv.remove();
-}
